24d36c9262987951b233c3fa51984c3070b777bc
Sergius <sergius@posteo.de>
2024-10-27 17:45:04 +0100

    fehlerbehebung

--- config.h
+++ config.h
@@ -3,12 +3,16 @@
 
 static const Block blocks[] = {
 	/* Symbol */	/* Befehl */		/* Intervall */		/* Aktualisierungssignal */
-	{"",		"sb-backup",		0,			20},
-	{"",		"sb-pacpackages",	0,			8},
-	/* {"",	"sb-kbselect",		0, 			30}, */
 	{"", 		"cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	{"",		"sb-help-icon",		60,			15},
+	{"",		"sb-backup",		60,			20},
+	{"",		"sb-handy",		60,			20},
+	{"",		"sb-clock",		60,			1},
+	{"",		"sb-moonphase",		18000,			17},
+	/* {"",		"sb-pacpackages",	0,			8}, */
+	/* {"",	"sb-kbselect",		0, 			30}, */
 	{"",		"sb-tasks",		10,			26},
-	/* {"",		"sb-music",		0,			11}, */
+	{"",		"sb-music",		0,			11},
 	{"",		"sb-news",		600,			6},
 	/* {"",		"sb-price xmr-btc \"Monero to Bitcoin\"  25",	9000,	25}, */
 	/* {"",		"sb-price xmr Monero  24",			9000,	24}, */
@@ -17,21 +21,18 @@ static const Block blocks[] = {
 	/*{"",		"sb-torrent",		20,			7}, */
 	/* {"",		"sb-memory",		10,			14}, */
 	/* {"",		"sb-cpu",		10,			18}, */
-	/*{"",		"sb-moonphase",		18000,			17}, */
 	/* {"",		"sb-doppler",		0,			13}, */
 	/* {"",		"sb-forecast",		18000,			5}, */
-	{"",		"sb-mailbox",		180,			12},
-	{"",		"sb-nettraf",		1,			16},
+	/* {"",		"sb-mailbox",		180,			12}, */
 	{"",		"sb-volume",		0,			10},
 	{"",		"sb-battery",		5,			3},
-	{"",		"sb-clock",		60,			1},
 	{"",		"sb-internet",		5,			4},
-	/* {"",		"sb-iplocate", 		0,			27}, */
-	{"",		"sb-help-icon",		0,			15},
+	{"",		"sb-nettraf",		1,			16},
+	{"",		"sb-iplocate", 		60,			15},
 };
 
 // Setzt das Trennzeichen zwischen Statusbefehlen. Das NULL-Zeichen ('\0') bedeutet kein Trennzeichen.
-static char *delim = " ";
+static char *delim = "|";
 
 // Lasse dwmblocks automatisch neu kompilieren und ausführen, wenn du diese Datei in
 // vim mit der folgenden Zeile in deiner vimrc/init.vim bearbeitest:
