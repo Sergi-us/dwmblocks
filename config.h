74467845eb5c917a75417b2443ce7cc5c46dacb6
Sergius M. <sergius@posteo.de>
2025-07-31 22:41:03 +0200

    statusbar maximiert

--- config.h
+++ config.h
@@ -4,37 +4,37 @@
 
 static const Block blocks[] = {
 /* Symbol */ /* Befehl */               /* Intervall */     /* Aktualisierungssignal */
-    {"",    "cat /tmp/recordingicon 2>/dev/null",   1,      1},
-    {"",    "sb-music",                 1,                  7},
-/*  {"",    "sb-price btc Bitcoin  21",    9000,           12},    */
-/*  {"",    "sb-yt",                    0,                  28},    */
-/*  {"",    "sb-mailbox",               3600,               20},    */
-/*  {"",   "sb-news",                  3600,               8},     */
-/*  {"",    "sb-backup",                60,                 2},     */
-    {" ",  "sb-pacpackages",           3600,               5},
-/*  {"",    "sb-tasks",                 10,                 6},     */
+    {"",    "cat /tmp/recordingicon 2>/dev/null",   1,          1},
+    {"",    "sb-music",                     1,                  7},
+    {"",    "sb-yt",                        0,                  28},
+    {"",    "sb-mailbox",                   600,                20},
+    {"",   "sb-news",                      600,                8},
+/*  {"",    "sb-backup",                    60,                 2},     */
+    {" ",  "sb-pacpackages",               0,                  5},
+/*  {"",    "sb-tasks",                     10,                 6},     */
+/*  {"",    "sb-torrent",                   20,                 13},    */
+/*  {"",    "sb-disk",                      10,                 16},    */
+/*  {"",    "sb-cpubars",                   1,                  17},    */
+/*  {"",    "sb-doppler",                   0,                  18},    */
+/*  {"",    "sb-forecast",                  18000,              19},    */
+    {"",    "sb-volume",                    60,                 21},
+/*  {"",    "sb-kbselect",                  0,                  25},    */
+/*  {"",    "sb-iplocate",                  120,                27},    */
+    {"",    "sb-xsct",                      3600,               30},
+    {"",    "sb-bluetooth",                 0,                  31},
+    {"",    "sb-hintergrund",               0,                  29},
+    {"",    "sb-ram",                       10,                 14},
+    {"",    "sb-cpu",                       10,                 15},
+    {"",    "sb-battery",                   60,                 22},
+    {"",    "sb-nettraf",                   1,                  24},
+    {"",    "sb-internet",                  5,                  23},
+/*  {"",    "sb-moonphase",                 18000,              4},     */
+    {"",    "sb-clock",                     60,                 26},
+    {"",    "sb-help-icon",                 0,                  3},
+/*  {"",    "sb-price btc Bitcoin  21",    9000,               12},    */
+    {"",    "sb-price xmr Monero  24",     9000,               10},
+/*  {"",    "sb-price eth Ethereum  23",   9000,               11},    */
 /*  {"",    "sb-price xmr-btc \"Monero to Bitcoin\"  25",	9000,	9}, */
-/*  {"",    "sb-price xmr Monero  24",     9000,           10},    */
-/*  {"",    "sb-price eth Ethereum  23",   9000,           11},    */
-/*  {"",    "sb-torrent",               20,                 13},    */
-/*  {"",    "sb-disk",                  10,                 16},    */
-/*  {"",    "sb-cpubars",               1,                  17},    */
-/*  {"",    "sb-doppler",               0,                  18},    */
-/*  {"",    "sb-forecast",              18000,              19},    */
-    {"",    "sb-volume",                60,                 21},
-/*  {"",    "sb-kbselect",              0,                  25},    */
-/*  {"",    "sb-iplocate",              120,                27},    */
-/*  {"",    "sb-moonphase",             18000,              4},     */
-/*  {"",    "sb-xsct",                  3600,               30},    */
-/*  {"",    "sb-bluetooth",             0,                  31},    */
-    {"",    "sb-hintergrund",           0,                  29},
-    {"",    "sb-ram",                   10,                 14},
-    {"",    "sb-cpu",                   10,                 15},
-    {"",    "sb-battery",               60,                 22},
-/*  {"",    "sb-nettraf",               1,                  24},    */
-    {"",    "sb-internet",              5,                  23},
-    {"",    "sb-clock",                 60,                 26},
-    {"",    "sb-help-icon",             0,                  3},
 };
 
 // Setzt das Trennzeichen zwischen Statusbefehlen. Das NULL-Zeichen ('\0') bedeutet kein Trennzeichen.
